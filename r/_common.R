08aa9361f41e4290a035e7808e9b48caea95d59b
janegit <jane_git@gitrdone.com>
2023-12-30 07:26:08 -0800

    add space

--- r/_common.R
+++ r/_common.R
@@ -1,5 +1,5 @@
 # General Course Details
-the_email     <- "gcook@CMC.edu"
+the_email        <- "gcook@CMC.edu"
 the_semester     <- "Fall 2024"
 the_course       <- "PSYC 166"
 the_section      <- paste(the_course, "Sect-01", sep = ", ")
@@ -7,6 +7,6 @@ the_day_time     <- "Tuesday 2:45 - 05:30PM (Pacific)"
 the_location     <- "Location: Roberts North, 105"
 the_office_hours <- "T: 1-2pm"
 the_instructor   <- "Gabriel I. Cook"
-the_contact      <- paste0("Discord or Email: ", the_email, "(*please put '", the_course," in subject line)*")
+the_contact      <- paste0("Discord or Email: ", the_email, " (*please put '", the_course," in subject line)*")
 the_credit       <- "3 hours; 1 credits"
 # End General Course Details
